5c108d207e2f288b8140e09b87a73393620ec6d2
hidetoraldy <hidetoraldy@gmail.com>
2023-02-28 15:01:07 +0700

    finalized

--- 3. arrow function/script.js
+++ 3. arrow function/script.js
@@ -65,3 +65,17 @@
 //     }
 // }
 
+const box = document.querySelector('.box');
+box.addEventListener('click', function(){
+    let satu = 'size';
+    let dua = 'caption';
+    //let temp = '';
+    if(this.classList.contains(satu)){
+        [satu, dua] = [dua, satu];
+    }
+
+    this.classList.toggle(satu);
+    setTimeout(() => {
+        this.classList.toggle(dua);
+    }, 600);
+});
