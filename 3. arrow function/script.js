4883025965e054570439af1d0d4f676f721b0f23
hidetoraldy <hidetoraldy@gmail.com>
2023-02-28 14:43:58 +0700

    adding style.css

--- 3. arrow function/script.js
+++ 3. arrow function/script.js
@@ -18,15 +18,50 @@
 // console.log(`Aldy Ramadhan`);
 //?
 
-let mahasiswa = ['Aldy Ramadhan', 'Aldy'];
+// let mahasiswa = ['Aldy Ramadhan', 'Aldy'];
 
-// let jumlahHuruf = mahasiswa.map(function(nama){
-//     return nama.length;
-// });
+// // let jumlahHuruf = mahasiswa.map(function(nama){
+// //     return nama.length;
+// // });
+// // console.log(jumlahHuruf);
+
+// // let jumlahHuruf = mahasiswa.map(nama => nama.length);
+// // console.log(jumlahHuruf);
+
+// let jumlahHuruf = mahasiswa.map(nama => ({ nama: nama, jmlHuruf: nama.length }));
 // console.log(jumlahHuruf);
 
-// let jumlahHuruf = mahasiswa.map(nama => nama.length);
-// console.log(jumlahHuruf);
+//? Constructor Function
+// const Mahasiswa = function(){
+//     this.nama = 'Aldy';
+//     this.umur = 24;
+//     this.sayHello = function(){
+//         console.log(`Halo nama saya ${this.nama} dan saya berumur ${this.umur} tahun`);
+//     }
+// }
 
-let jumlahHuruf = mahasiswa.map(nama => ({ nama: nama, jmlHuruf: nama.length }));
-console.log(jumlahHuruf);
+// const aldy = new Mahasiswa();
+
+//? Arrow Function
+// const Mahasiswa = function(){
+//     this.nama = 'Aldy';
+//     this.umur = 24;
+//     this.sayHello = () => {
+//         console.log(`Halo nama saya ${this.nama} dan saya berumur ${this.umur} tahun`);
+//     }
+
+//     setInterval(() => {
+//         console.log(this.umur++);
+//     }, 500);
+// }
+
+// const aldy = new Mahasiswa();
+
+// const mhs1 = {
+//     nama: `Aldy`,
+//     umur: 24,
+//     sayHello: () => {
+//         console.log(`Halo nama saya ${this.nama} dan saya berumur ${this.umur} tahun`);
+//     }
+// }
+
