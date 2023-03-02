53911a3179002a49e97e84bb92c7e159d6c1baea
hidetoraldy <hidetoraldy@gmail.com>
2023-03-02 10:55:53 +0700

    finalized

--- 5.1 latihan/script.js
+++ 5.1 latihan/script.js
@@ -1,12 +1,78 @@
-//TODO HTML Fragments
+//TODO 1. HTML Fragments
+// const mhs = {
+//     nama: 'Aldy Ramadhan',
+//     umur: 24,
+//     nim: '10517082',
+//     email: 'hidetoraldy@gmail.com'
+// };
+
+// const el = `<div class="mhs">
+//     <h2>${mhs.nama}</h2>
+//     <span class="nim">${mhs.nim}</span>
+// </div>`
+
+//TODO 2. Looping
+// const mhs = [
+//     {
+//         nama: 'Aldy Ramadhan',
+//         email: 'hidetoraldy@gmail.com'
+//     },
+//     {
+//         nama: 'Aldy',
+//         email: 'hidetoraldy@gmail.com'
+//     },
+//     {
+//         nama: 'Ramadhan',
+//         email: 'hidetoraldy@gmail.com'
+//     }
+// ];
+
+// const el = `<div class="mhs">
+//     ${mhs.map(m => `<ul>
+//         <li>${m.nama}</li>
+//         <li>${m.email}</li>
+//     </ul>`).join('')}
+// </div>`
+
+//TODO 3. Conditionals
+//? ternary
+// const lagu = {
+//     judul: `Wicked`,
+//     penyanyi: 'Hoshimachi Suisei',
+//     feat: 'Mori Calliope'
+// };
+
+// const el = `<div class="lagu">
+//     <ul>
+//         <li>${lagu.judul}</li>
+//         <li>${lagu.penyanyi} ${lagu.feat ? `(feat. ${lagu.feat})` : ''}</li>
+//     </ul>
+// </div>`
+
+//TODO 4. Nested HTML Fragments
 const mhs = {
     nama: 'Aldy Ramadhan',
-    umur: 24,
-    nim: '10517082',
-    email: 'hidetoraldy@gmail.com'
+    semester: 8,
+    mataKuliah: [
+        'Skripsi',
+        'Attitude dan Perilaku',
+        'Kewirausahaan'
+    ]
 };
 
-const el = `<div class="mhs">
+function cetakMataKuliah(mataKuliah){
+    return `
+        <ol>
+            ${mataKuliah.map(mk => `<li>${mk}</li>`).join('')}
+        </ol>
+    `
+}
+
+const el = `<div class="lagu">
     <h2>${mhs.nama}</h2>
-    <span class="nim">${mhs.nim}</span>
+    <span class="semester">Semester : ${mhs.semester}</span>
+    <h4>Mata Kuliah :</h4>
+    ${cetakMataKuliah(mhs.mataKuliah)}
 </div>`
+
+document.body.innerHTML = el;
