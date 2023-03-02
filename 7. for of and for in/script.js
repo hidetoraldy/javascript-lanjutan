0d6a9c08242bafa81ca825184ee653dd8da0fbd2
hidetoraldy <hidetoraldy@gmail.com>
2023-03-02 15:07:32 +0700

    on pause

---
+++ 7. for of and for in/script.js
@@ -0,0 +1,23 @@
+//TODO for ..of
+// const mhs = ['Aldy Ramadhan', 'Aldy', 'Ramadhan'];
+
+// // for (let i = 0; i < mhs.length; i++){
+// //     console.log(mhs[i]);
+// // }
+
+// // mhs.forEach(m => console.log(m));
+
+// for(const m of mhs ){
+//     console.log(m);
+// }
+
+//? String
+// const nama = 'Aldy';
+// for(const n of nama){
+//     console.log(n);
+// }
+
+const mhs = ['Aldy Ramadhan', 'Aldy', 'Ramadhan'];
+// mhs.forEach((m, i) => {
+//     console.log(`${m} adalah mahasiswa ke ${i + 1}`);
+// });
