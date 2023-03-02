edc9e2de9b3613c76ef03dce7ca49ef2a61ee026
hidetoraldy <hidetoraldy@gmail.com>
2023-03-02 10:33:35 +0700

    initial commit

---
+++ 5.1 latihan/script.js
@@ -0,0 +1,12 @@
+//TODO HTML Fragments
+const mhs = {
+    nama: 'Aldy Ramadhan',
+    umur: 24,
+    nim: '10517082',
+    email: 'hidetoraldy@gmail.com'
+};
+
+const el = `<div class="mhs">
+    <h2>${mhs.nama}</h2>
+    <span class="nim">${mhs.nim}</span>
+</div>`
