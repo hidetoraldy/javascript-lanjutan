c11a1e2784f6d690609198a7ca7cb82d6fd11195
hidetoraldy <hidetoraldy@gmail.com>
2023-03-02 11:35:03 +0700

    finalized

--- 5.2 Tag Templates/script.js
+++ 5.2 Tag Templates/script.js
@@ -1,5 +1,29 @@
 //TODO Tagged Templates
+// const nama = 'Aldy';
+// const umur = 24;
+
+// function coba(strings, ...values){
+//     // let result = '';
+//     // strings.forEach((str, i) => {
+//     //     result += `${str}${values[i] || ''}`
+//     // })
+//     // return result;
+
+//     return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}`, '')
+// }
+
+// const str = coba`Halo! nama saya ${nama}, saya berumur ${umur} tahun.`;
+// console.log(str);
+
+//? Highlight
 const nama = 'Aldy';
 const umur = 24;
+const email = 'hidetoraldy@gmail.com'
 
-const str = `Halo! nama saya ${nama}, saya berumur ${umur} tahun.`;
+function highlight(strings, ...values){
+    return strings.reduce((result, str, i) => `${result}${str}<span class="hl">${values[i] || ''}</span>`, '')
+}
+
+const str = highlight`Halo! nama saya ${nama}, saya berumur ${umur} tahun, dan email saya ${email}`;
+// console.log(str);
+document.body.innerHTML = str;
