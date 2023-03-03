2c529a7d4c6a19e336030cb1322a8b7d2baba1b6
hidetoraldy <hidetoraldy@gmail.com>
2023-03-03 09:49:23 +0700

    finalized

---
+++ 8 Spread Operator/script.js
@@ -0,0 +1,18 @@
+//TODO Spread Operator
+//? memecah iterables menjadi single element
+
+// const mhs = ['Aldy', 'Ramadhan'];
+// console.log(...mhs[0]);
+
+//? penggunaan spread operator pada kasus menggabungkan 2 array
+// const mhs = ['Aldy', 'Ramadhan'];
+// const agen = ['phoenix', 'neon'];
+// const orang = [...mhs, ...agen];
+// console.log(orang);
+
+//? meng-copy array
+// const mhs = ['Aldy', 'Ramadhan'];
+// const mhs1 = [...mhs];
+// mhs1[0] = 'null';
+// console.log(mhs);
+// console.log(mhs1);
