a6984ebbcd400fa7f6c934a27d1045a3497659eb
hidetoraldy <hidetoraldy@gmail.com>
2023-03-10 09:22:23 +0700

    fix

--- 10. Callback/script.js
+++ 10. Callback/script.js
@@ -33,11 +33,34 @@
 // mhs.forEach(m => console.log(m.nama));
 
 //? Asynchronus Callback
-//* JQuery
-console.log('mulai');
-$.ajax({
-    url: 'mahasiswa.json',
-    success: (mhs) => {
-        mhs.forEach()
+//* Vanilla Javascript
+function getDataMahasiswa(url, success, error){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                success(xhr.response);
+            } else if(xhr.status === 404){
+                error();
+            }
+        }
     }
+
+    xhr.open('get', url);
+    xhr.send();
+}
+
+getDataMahasiswa('data/mahasiswa.json', results => {
+    console.log(results);
+}, () => {
+    
 });
+//* JQuery
+// console.log('mulai');
+// $.ajax({
+//     url: 'mahasiswa.json',
+//     success: (mhs) => {
+//         mhs.forEach()
+//     }
+// });
